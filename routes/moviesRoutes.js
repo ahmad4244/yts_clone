434d3290c8b08763e162951145d56bba14769cc6
M. Ahmad <136705313+ahma97@users.noreply.github.com>
2025-06-03 19:00:18 +0500

    updated

--- routes/moviesRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getMovies, searchMovies } from "../controllers/moviesController.js";
-
-const router = express.Router();
-
-router.get("/movies", getMovies);
-router.get("/search", searchMovies);
-
-export default router;
